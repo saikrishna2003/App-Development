368d055f4d3496033b1b75f249c624d02709a0da
Saikrishna.V <sai.official08@gmail.com>
2023-10-05 21:40:00 +0530

    First Commit
    First Commit

---
+++ Day 15/src/main/java/com/ecommerce/controller/BooksController.java
@@ -0,0 +1,42 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.entity.Books;
+import com.ecommerce.repository.BooksRepository;
+import com.ecommerce.service.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BooksController {
+    @Autowired
+    BooksRepository r1;
+
+    @Autowired
+    BooksService s1;
+
+    @GetMapping("/books/get")
+    public List<Books> getDetails() {
+        return s1.getDetails();
+    }
+
+    @PostMapping("/books/post")
+    public String postDetails(@RequestBody Books m1) {
+        return s1.postDetails(m1);
+    }
+
+    @PutMapping("/books/put/{bid}")
+    public Books updateDetails(@RequestBody Books m)
+    {
+        return s1.updateDetails(m);
+    }
+
+    @DeleteMapping("/books/delete/{id}")
+    public void deleteDetails(@PathVariable("id")  Long id)
+    {
+       s1.deleteDetails(id);
+    }
+
+
+}
---
+++ Day 15/src/main/java/com/ecommerce/entity/Books.java
@@ -0,0 +1,75 @@
+package com.ecommerce.entity;
+
+import com.ecommerce.entity.enumerate.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "_books")
+public class Books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bid;
+    private String bookname;
+    private String bookauthor;
+    private String edition;
+
+    @ManyToOne
+    @JoinColumn(name = "booksCategory")
+    private BooksCategory booksCategory;
+
+    public Long getBid() {
+        return bid;
+    }
+
+    public void setBid(Long bid) {
+        this.bid = bid;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getBookauthor() {
+        return bookauthor;
+    }
+
+    public void setBookauthor(String bookauthor) {
+        this.bookauthor = bookauthor;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public BooksCategory getBooksCategory() {
+        return booksCategory;
+    }
+
+    public void setBooksCategory(BooksCategory booksCategory) {
+        this.booksCategory = booksCategory;
+    }
+
+    public Books(Long bid, String bookname, String bookauthor, String edition, BooksCategory booksCategory) {
+        this.bid = bid;
+        this.bookname = bookname;
+        this.bookauthor = bookauthor;
+        this.edition = edition;
+        this.booksCategory = booksCategory;
+    }
+
+    public Books() {
+
+    }
+}
