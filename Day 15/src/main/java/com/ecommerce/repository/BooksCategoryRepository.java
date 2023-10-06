62bd3d25351b2b8078f905a1fcddde9b0837cfdf
Saikrishna.V <sai.official08@gmail.com>
2023-10-06 09:31:46 +0530

    Delete

--- Day 15/src/main/java/com/ecommerce/repository/BooksCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.BooksCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BooksCategoryRepository extends JpaRepository<BooksCategory,Integer> {
-}
--- Day 15/src/main/java/com/ecommerce/repository/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BooksRepository extends JpaRepository<Books,Long> {
-}
