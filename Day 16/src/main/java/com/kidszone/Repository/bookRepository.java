0eae09bd2022c18f8401ecee1009a19367d8447e
Saikrishna.V <sai.official08@gmail.com>
2023-10-09 11:40:26 +0530

    Delete

--- Day 16/src/main/java/com/kidszone/Repository/bookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kidszone.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kidszone.Entity.Books;
-
-public interface bookRepository extends JpaRepository<Books, Long>{
-
-}
--- Day 16/src/main/java/com/kidszone/Repository/userRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.kidszone.Repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.kidszone.Entity.User;
-//
-//public interface userRepository extends JpaRepository<User, Long>{
-//
-//}
